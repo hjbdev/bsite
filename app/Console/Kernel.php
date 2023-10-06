935096f247646c35c5e4ab028cafeb97445943e7
Harry (hjbdev) <harry@hjb.dev>
2023-10-06 23:03:04 +0100

    fix: nav z index

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -14,6 +14,7 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')->hourly();
         $schedule->command('horizon:snapshot')->everyFiveMinutes();
+        $schedule->command('telescope:prune')->daily();
     }
 
     /**
