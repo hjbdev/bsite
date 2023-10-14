f5a9451177a55da53863c2ca6d05b53c3fd20a11
Harry (hjbdev) <harry@hjb.dev>
2023-10-14 14:07:27 +0100

    feat: defusing event

---
+++ resources/js/Components/Series/MatchFeedItems/BombDefusing.vue
@@ -0,0 +1,12 @@
+<script setup>
+import BaseMatchFeedItem from "./BaseMatchFeedItem.vue";
+
+defineProps({
+    log: Object,
+});
+</script>
+<template>
+    <BaseMatchFeedItem class="border-blue-500">
+        {{ log.data.userName }} is defusing the bomb
+    </BaseMatchFeedItem>
+</template>
