a89cabfda64f603cf4ce0010a23db73dba9593bf
Harry (hjbdev) <harry@hjb.dev>
2023-10-14 11:51:52 +0100

    fix: homepage event query

---
+++ app/Console/Commands/Players/ImportPlayersFromSpreadsheet.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Console\Commands\Players;
+
+use Illuminate\Console\Command;
+
+class ImportPlayersFromSpreadsheet extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:import-players-from-spreadsheet';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Import players from ukcs spreadsheet.';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        
+    }
+}
