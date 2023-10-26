d6d465c569cd15c88287873b22bb7e35845ed3c8
Harry <harry@HarryL>
2023-10-26 22:54:05 +0100

    feat: epiclan master sheet import

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,6 +17,6 @@ class DatabaseSeeder extends Seeder
         if (env('APP_ENV') === 'production') {
             return;
         }
-        app(TestDataSeeder::class)->run();
+        // app(TestDataSeeder::class)->run();
     }
 }
