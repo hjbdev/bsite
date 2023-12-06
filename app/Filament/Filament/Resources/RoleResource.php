c27e945c097eddbaa6b0f9304cb0d5cb3ba0c715
Harry (hjbdev) <harry@hjb.dev>
2023-12-06 17:00:21 +0000

    fix: role policy, role ui

---
+++ app/Filament/Filament/Resources/RoleResource.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Filament\Filament\Resources;
+
+use App\Filament\Filament\Resources\RoleResource\Pages;
+use App\Filament\Filament\Resources\RoleResource\RelationManagers;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Silber\Bouncer\Database\Role;
+
+class RoleResource extends Resource
+{
+    protected static ?string $model = Role::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('name')
+                    ->autofocus()
+                    ->required()
+                    ->unique(Role::class, 'name'),
+                Forms\Components\TextInput::make('title')
+                    ->required(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('name'),
+                TextColumn::make('title'),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            RelationManagers\AbilitiesRelationManager::class,
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListRoles::route('/'),
+            'create' => Pages\CreateRole::route('/create'),
+            'edit' => Pages\EditRole::route('/{record}/edit'),
+        ];
+    }
+}
---
+++ app/Filament/Filament/Resources/RoleResource/Pages/CreateRole.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Filament\Resources\RoleResource\Pages;
+
+use App\Filament\Filament\Resources\RoleResource;
+use Filament\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateRole extends CreateRecord
+{
+    protected static string $resource = RoleResource::class;
+}
