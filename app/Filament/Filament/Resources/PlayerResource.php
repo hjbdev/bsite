9d5ec7e585cef46ad72bb4b173ca4c949f89ec75
Harry (hjbdev) <harry@hjb.dev>
2023-12-06 13:53:13 +0000

    fix: whoops2

--- app/Filament/Filament/Resources/PlayerResource.php
+++ app/Filament/Filament/Resources/PlayerResource.php
@@ -45,7 +45,7 @@ class PlayerResource extends Resource
                 TextColumn::make('updated_at')
                     ->date(),
             ])
-            ->defaultSort('updated_at', 'desc)
+            ->defaultSort('updated_at', 'desc')
             ->filters([
                 //
             ])
