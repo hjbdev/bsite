50a131b8644e7c8823b44a684a6a6e208825eb9b
Harry (hjbdev) <harry@hjb.dev>
2023-12-06 16:23:55 +0000

    fix: backend redirect

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -18,7 +18,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/admin/dashboard';
+    public const HOME = '/admin';
 
     /**
      * Define your route model bindings, pattern filters, and other route configuration.
