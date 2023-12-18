350e2ab9dd7ea5acd5202163037be641a40b3b7b
Harry (hjbdev) <harry@hjb.dev>
2023-12-18 16:41:51 +0000

    fix: entire echo initialization inside ssr check

--- resources/js/Composables/useEcho.js
+++ resources/js/Composables/useEcho.js
@@ -1,26 +1,28 @@
 import Echo from "laravel-echo";
 import Pusher from "pusher-js";
 
-if (typeof window !== 'undefined') {
-    window.Pusher = Pusher;
-}
+let echo = null;
 
-const echo = new Echo({
-    broadcaster: "pusher",
-    key: import.meta.env.VITE_PUSHER_APP_KEY,
-    // wsHost: 'soketi', // testing
-    wsHost:
-        import.meta.env.VITE_PUSHER_HOST === "soketi"
-            ? "localhost"
-            : import.meta.env.VITE_PUSHER_HOST, // everything else
-    wsPort: import.meta.env.VITE_PUSHER_PORT,
-    wssPort: import.meta.env.VITE_PUSHER_PORT,
-    cluster: "eu",
-    forceTLS: false,
-    encrypted: true,
-    disableStats: true,
-    // enabledTransports: ["ws", "wss"],
-});
+if (typeof window !== "undefined") {
+    window.Pusher = Pusher;
+
+    echo = new Echo({
+        broadcaster: "pusher",
+        key: import.meta.env.VITE_PUSHER_APP_KEY,
+        // wsHost: 'soketi', // testing
+        wsHost:
+            import.meta.env.VITE_PUSHER_HOST === "soketi"
+                ? "localhost"
+                : import.meta.env.VITE_PUSHER_HOST, // everything else
+        wsPort: import.meta.env.VITE_PUSHER_PORT,
+        wssPort: import.meta.env.VITE_PUSHER_PORT,
+        cluster: "eu",
+        forceTLS: false,
+        encrypted: true,
+        disableStats: true,
+        // enabledTransports: ["ws", "wss"],
+    });
+}
 
 export default function useEcho() {
     return echo;
