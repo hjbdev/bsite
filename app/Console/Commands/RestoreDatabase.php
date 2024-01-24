eed0fd9158095d18a265cfdf6addc98bd49b0d32
Harry (hjbdev) <harry@hjb.dev>
2024-01-24 16:26:54 +0000

    Add storage/test-data and database/*.sql to .gitignore

---
+++ app/Console/Commands/RestoreDatabase.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class RestoreDatabase extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:restore-database {file}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        //
+    }
+}
